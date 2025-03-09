08ae2e538ccd1e93ffdcc6907bf75cfc1cc9ef53
danie <danie@D_SANTILLAN_PC>
2025-03-08 19:34:29 -0500

    Build information panel with text fields for necessary drawing info

--- MyFractal/src/fractal/InformationPanel.java
+++ MyFractal/src/fractal/InformationPanel.java
@@ -1,13 +1,151 @@
 package fractal;
 
 import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
 
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 public class InformationPanel extends JPanel {
+	
+	private static final long serialVersionUID = 1L;
+	private JTextField numSidesInput;
+	private JTextField minEdgeSizeInput;
+	private JTextField maxEdgeSizeInput;
+	private JTextField timerPauseInput;
+	private JCheckBox drawShape;
+	private JCheckBox drawFractal;
+	
 
-	public InformationPanel() {
-		setBackground(Color.WHITE);
+	public InformationPanel(JButton draw, JButton clear) {
+		setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+		Color bgColor = new Color(220, 220, 220);
+		setBackground(bgColor);
+		
+		Font helveticaBold = new Font("Helvetica", Font.BOLD, 17);
+		Font helveticaPlain = new Font("Helvetica", Font.PLAIN, 17);
+		Dimension textFieldDimension = new Dimension(100, 35);
+		
+		JLabel numSides = new JLabel("Number of Edges (Range 3-6)");
+		numSides.setFont(helveticaBold);
+		numSidesInput = new JTextField();
+		numSidesInput.setText("4");
+		numSidesInput.setFont(helveticaPlain);
+		numSidesInput.setMinimumSize(numSidesInput.getPreferredSize());
+		numSidesInput.setPreferredSize(textFieldDimension);
+		numSidesInput.setMaximumSize(numSidesInput.getPreferredSize());
+		
+		JLabel minEdgeSize = new JLabel("Minimum Edge Size");
+		minEdgeSize.setFont(helveticaBold);
+		minEdgeSizeInput = new JTextField();
+		minEdgeSizeInput.setText("10");
+		minEdgeSizeInput.setFont(helveticaPlain);
+		minEdgeSizeInput.setMinimumSize(minEdgeSizeInput.getPreferredSize());
+		minEdgeSizeInput.setPreferredSize(textFieldDimension);
+		minEdgeSizeInput.setMaximumSize(minEdgeSizeInput.getPreferredSize());
+		
+		JLabel maxEdgeSize = new JLabel("Base Edge Size");
+		maxEdgeSize.setFont(helveticaBold);
+		maxEdgeSizeInput = new JTextField();
+		maxEdgeSizeInput.setText("200");
+		maxEdgeSizeInput.setFont(helveticaPlain);
+		maxEdgeSizeInput.setMinimumSize(maxEdgeSizeInput.getPreferredSize());
+		maxEdgeSizeInput.setPreferredSize(textFieldDimension);
+		maxEdgeSizeInput.setMaximumSize(maxEdgeSizeInput.getPreferredSize());
+		
+		JLabel timerPause = new JLabel("Timer Pause Time");
+		timerPause.setFont(helveticaBold);
+		timerPauseInput = new JTextField();
+		timerPauseInput.setText("100");
+		timerPauseInput.setFont(helveticaPlain);
+		timerPauseInput.setMinimumSize(timerPauseInput.getPreferredSize());
+		timerPauseInput.setPreferredSize(textFieldDimension);
+		timerPauseInput.setMaximumSize(timerPauseInput.getPreferredSize());
+		
+		drawShape = new JCheckBox("Draw Shape");
+		drawShape.setSelected(true);
+		drawShape.setFont(helveticaBold);
+		drawShape.setBackground(bgColor);
+		
+		drawFractal = new JCheckBox("Draw Fractal");
+		drawFractal.setFont(helveticaBold);
+		drawFractal.setBackground(bgColor);
+		
+		draw.setFont(helveticaBold);
+		clear.setFont(helveticaBold);
+		
+		add(numSides);
+		add(numSidesInput);
+		add(Box.createRigidArea(new Dimension(0, 20)));
+		add(minEdgeSize);
+		add(minEdgeSizeInput);
+		add(Box.createRigidArea(new Dimension(0, 20)));
+		add(maxEdgeSize);
+		add(maxEdgeSizeInput);
+		add(Box.createRigidArea(new Dimension(0, 20)));
+		add(timerPause);
+		add(timerPauseInput);
+		add(Box.createRigidArea(new Dimension(0, 20)));
+		add(drawShape);
+		add(Box.createRigidArea(new Dimension(0, 20)));
+		add(drawFractal);
+		add(Box.createRigidArea(new Dimension(0, 20)));
+		add(draw);
+		add(Box.createRigidArea(new Dimension(0, 20)));
+		add(clear);
+		
+	}
+	
+	
+	public int getNumSides() {
+		try {
+			return Integer.parseInt(numSidesInput.getText());
+		} catch (Exception e) {
+			return -1;
+		}
+	}
+	
+	
+	public int getMinEdgeSize() {
+		try {
+			return Integer.parseInt(minEdgeSizeInput.getText());
+		} catch (Exception e) {
+			return -1;
+		}
+	}
+	
+	
+	public int getMaxEdgeSize() {
+		try {
+			return Integer.parseInt(maxEdgeSizeInput.getText());
+		} catch (Exception e) {
+			return -1;
+		}
+	}
+
+	
+	public int getTimerPause() {
+		try {
+			return Integer.parseInt(timerPauseInput.getText());
+		} catch (Exception e) {
+			return -1;
+		}
+	}
+	
+	
+	public boolean isShapeDrawn() {
+		return drawShape.isSelected();
+	}
+	
+	
+	public boolean isFractalDrawn() {
+		return drawFractal.isSelected();
 	}
 	
 	
--- MyFractal/src/fractal/RunMyFractal.java
+++ MyFractal/src/fractal/RunMyFractal.java
@@ -2,24 +2,36 @@ package fractal;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 public class RunMyFractal {
 	
 	private static final long serialVersionUID = 1L;
+	private int width;
+	private int height;
+	private int availableWidth;
 	
 
 	public RunMyFractal() {
-		createFrame();
+		createFrame(1000, 900);
 	}
 	
 	
-	private void createFrame() {
+	private void createFrame(int givenWidth, int givenHeight) {
+		width = givenWidth;
+		height = givenHeight;
+		availableWidth = givenWidth - 300;
+		
 		JFrame frame = new JFrame("Daniel's Fractal");
-		frame.setSize(1000, 900);
+		frame.setSize(width, height);
 		ImageIcon icon = new ImageIcon(getClass().getClassLoader().getResource("IMAGE_ICON.png"));
 		frame.setIconImage(icon.getImage());
 		frame.setResizable(false);
@@ -27,7 +39,48 @@ public class RunMyFractal {
 		
 		JPanel drawingPanel = new JPanel();
 		drawingPanel.setBackground(Color.BLACK);
-		InformationPanel infoPanel = new InformationPanel();
+		
+		JButton drawButton = new JButton("Draw!");
+		JButton clearButton = new JButton("Clear");
+		InformationPanel infoPanel = new InformationPanel(drawButton, clearButton);
+		
+		drawButton.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				int numSides = infoPanel.getNumSides();
+				int minEdgeSize = infoPanel.getMinEdgeSize();
+				int maxEdgeSize = infoPanel.getMaxEdgeSize();
+				int timerPause = infoPanel.getTimerPause();
+				boolean isShapeDrawn = infoPanel.isShapeDrawn();
+				boolean isFractalDrawn = infoPanel.isFractalDrawn();
+				
+				if (numSides <= 2 || numSides > 6) {
+					JOptionPane.showMessageDialog(null, "Number of sides must be between 3 and 6.",
+							"Failed to Run", JOptionPane.ERROR_MESSAGE);
+					return;
+				} else if (minEdgeSize < 2) {
+					JOptionPane.showMessageDialog(null, "Minimum edge side is too small... we cannot cause stack overflow, ya know.",
+							"Failed to Run", JOptionPane.ERROR_MESSAGE);
+					return;
+				} else if (minEdgeSize > maxEdgeSize) {
+					JOptionPane.showMessageDialog(null, "Minimum edge size is larger than maximum edge size.",
+							"Failed to Run", JOptionPane.ERROR_MESSAGE);
+					return;
+				} else if (maxEdgeSize > 500) {
+					JOptionPane.showMessageDialog(null, "Maximum edge side is too large ... your device may not show the entire shape.",
+							"Failed to Run", JOptionPane.ERROR_MESSAGE);
+					return;
+				}
+				
+				width = 1000 + 3*(maxEdgeSize-200);
+				height = 900 + 2*(maxEdgeSize-200);
+				availableWidth = 700 + 3*(maxEdgeSize-200);
+				frame.setSize(width, height);
+				
+			}
+			
+		});
 		
 		frame.add(drawingPanel, BorderLayout.CENTER);
 		frame.add(infoPanel, BorderLayout.EAST);
